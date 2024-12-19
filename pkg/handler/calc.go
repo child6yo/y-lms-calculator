ce3c20d7891de764bd1812a3d46e09329320c07c
Arseniy Glushkov <miguelprod8@gmail.com>
2024-12-20 00:58:31 +0700

    feat: handler request and response

--- pkg/handler/calc.go
+++ pkg/handler/calc.go
@@ -1,12 +1,14 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
 type RequestModel struct {
-	Expression string `json:"name"`
+	Expression string `json:"expression"`
 }
 
 type ResponseModel struct {
@@ -14,6 +16,22 @@ type ResponseModel struct {
 }
 
 func (h *Handler) CalculatorHandler(w http.ResponseWriter, r *http.Request) {
-	n, _ := h.services.Calculator.Calc("2+2*2")
-	fmt.Fprint(w, n)
+	var s RequestModel
+
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+	defer r.Body.Close()
+
+	json.Unmarshal(data, &s)
+
+	res, _ := h.services.Calculator.Calc(s.Expression)
+	response := ResponseModel{Result: fmt.Sprintf("%v", res)}
+	responseData, err := json.Marshal(response)
+	if err != nil { 
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError) 
+	}
+	w.Header().Set("Content-Type", "application/json") 
+	w.Write(responseData)
 }
